96d5ed86c41f8d090554091f7e0c2ca565288d09
Maarten Demeyer <maarten.demeyer@oakbrookfinance.com>
2020-12-15 09:19:39 +0000

    day 14 improved

--- aoc20_day14.jl
+++ aoc20_day14.jl
@@ -1,5 +1,5 @@
-## Advent of Code 2020, Day 11
-## https://adventofcode.com/2020/day/11
+## Advent of Code 2020, Day 14
+## https://adventofcode.com/2020/day/14
 ##
 ## Author: Maarten Demeyer <mpjdem@gmail.com>
 ## GitHub: https://github.com/mpjdem
@@ -8,63 +8,65 @@
 include("common.jl")
 
 struct Instruction 
-    bitmask::String
+    bitmask::BitArray{1}
+    xmask::BitArray{1}
     memints::Array{Tuple{Int,Int}, 1}
 end
 
-inp = open("input/input14.txt", "r") do file
+instructions = open("input/input14.txt", "r") do file
     lns = readlines(file)
     instructions = Array{Instruction, 1}(undef, 0)
-    bitmask = ""
+    bitmask = BitArray{1}(undef, 0)
+    xmask = BitArray{1}(undef, 0)
     memints = Array{Tuple{Int, Int}, 1}(undef, 0)
     for ln in lns
         if startswith(ln, "mask")
             if !isempty(memints) 
-                push!(instructions, Instruction(bitmask, memints))
+                push!(instructions, Instruction(bitmask, xmask, memints))
             end
-            bitmask = split(ln, " = ")[2]
+            maskarr = reverse(split(match(r"mask = ([0X1]+)", ln)[1], ""))
+            bitmask = maskarr .== "1"
+            xmask = maskarr .== "X"
             memints = Array{Tuple{Int, Int}, 1}(undef, 0)
         else
-            tmp = split(ln, ['[', ']', ' ', '='])
-            push!(memints, (parse(Int, tmp[2]), parse(Int, tmp[6])))
+            ints = parse.(Int, [x.match for x in eachmatch(r"([0-9]+)", ln)])
+            push!(memints, Tuple(ints))
         end 
     end
-    push!(instructions, Instruction(bitmask, memints))
+    push!(instructions, Instruction(bitmask, xmask, memints))
     instructions
 end
 
+function int2bit(x::Int, pad::Int)::BitArray{1}
+    digits(x, base = 2, pad = pad) .== 1
+end
+
+function bit2int(x::BitArray{1})::Int
+    sum((2 .^ (0:(length(x) - 1))) .* x)
+end
+
 ## -- PART 1 --
+masksz = length(instructions[1].bitmask)
 addresses = Dict()
-for instr in inp, memint in instr.memints
-    int_as_bits = digits(memint[2], base = 2, pad = length(instr.bitmask))
-    bm_as_arr = reverse(split(instr.bitmask, ""))
-    for j in 1:length(bm_as_arr)
-        if bm_as_arr[j] == "1"
-            int_as_bits[j] = 1
-        elseif bm_as_arr[j] == "0"
-            int_as_bits[j] = 0
-        end
-    end
-    addresses[memint[1]] = sum((2 .^ (0:(length(instr.bitmask) - 1))) .* int_as_bits)
+for instr in instructions, memint in instr.memints
+    bval = int2bit(memint[2], masksz)
+    bval[.!instr.xmask] = instr.bitmask[.!instr.xmask]
+    addresses[memint[1]] = bit2int(bval)
 end
 
 solution_1 = sum(values(addresses))
 
 ## -- PART 2 --
+## Floating bit combinations are generated as 1:2^nbits in binary
 addresses = Dict()
-for instr in inp, memint in instr.memints
-    maddr_as_bits = digits(memint[1], base = 2, pad = length(instr.bitmask))
-    bm_as_arr = reverse(split(instr.bitmask, ""))
-    xidx = findall(bm_as_arr .== "X")
-    bm_as_arr = reshape(repeat(bm_as_arr, 2^length(xidx)), 
-                        length(instr.bitmask), 2^length(xidx))
-    for repi in 0:(size(bm_as_arr)[2] - 1)
-        replbits = digits(repi, base = 2, pad = length(xidx))
-        macp = copy(maddr_as_bits)
-        macp[xidx] = replbits
-        macp[bm_as_arr[:, repi + 1] .== "1"] .= 1
-        ma_int = sum((2 .^ (0:(length(instr.bitmask) -1 ))) .* macp)
-        addresses[ma_int] = memint[2]
+for instr in instructions, memint in instr.memints
+    y = int2bit(memint[1], masksz)
+    nbits = sum(instr.xmask)
+    for floatint in 0:(2^nbits - 1)
+        floatbits = int2bit(floatint, nbits)
+        y[instr.xmask] = floatbits
+        y[instr.bitmask] .= true 
+        addresses[bit2int(y)] = memint[2]
     end
 end
 
